8a2e718634798122384e1472e763a4abf38b7fe6
sachin verma <sv2834766@gmail.com>
2025-11-20 18:17:46 +0530

    Reapply "Initial project structure"
    This reverts commit f5ad425738ed8a961f5550e7d3171d209fd07a95.

---
+++ src/test/java/com/freemiumwear/FreemiumwearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.freemiumwear;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FreemiumwearBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
