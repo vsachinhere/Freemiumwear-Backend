2682a7567ef19ac6d7e100aef0def963a1808af7
sachin verma <sv2834766@gmail.com>
2025-11-20 19:01:22 +0530

    resolved conflicts

--- src/main/java/com/freemiumwear/config/OpenApiConfig.java
+++ src/main/java/com/freemiumwear/config/OpenApiConfig.java
@@ -1,17 +1,17 @@
-package com.freemiumwear.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
-import java.util.ArrayList;
-
-@Configuration
-public class OpenApiConfig {
-    public OpenApiConfig(MappingJackson2HttpMessageConverter converter) {
-        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
-        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
-        converter.setSupportedMediaTypes(supportedMediaTypes);
-    }
-
-}
+//package com.freemiumwear.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//
+//import java.util.ArrayList;
+//
+//@Configuration
+//public class OpenApiConfig {
+//    public OpenApiConfig(MappingJackson2HttpMessageConverter converter) {
+//        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+//        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+//        converter.setSupportedMediaTypes(supportedMediaTypes);
+//    }
+//
+//}
--- src/main/java/com/freemiumwear/config/SwaggerConfig.java
+++ src/main/java/com/freemiumwear/config/SwaggerConfig.java
@@ -1,32 +1,32 @@
-package com.freemiumwear.config;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("Compliance API's")
-                                .version("1.0")
-                                .description("API documentation for Compliance Application"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(
-                                        "bearerAuth",
-                                        new SecurityScheme()
-                                                .name("bearerAuth")
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")));
-    }
-}
+//package com.freemiumwear.config;
+//
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(
+//                        new Info()
+//                                .title("Compliance API's")
+//                                .version("1.0")
+//                                .description("API documentation for Compliance Application"))
+//                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+//                .components(
+//                        new Components()
+//                                .addSecuritySchemes(
+//                                        "bearerAuth",
+//                                        new SecurityScheme()
+//                                                .name("bearerAuth")
+//                                                .type(SecurityScheme.Type.HTTP)
+//                                                .scheme("bearer")
+//                                                .bearerFormat("JWT")));
+//    }
+//}
